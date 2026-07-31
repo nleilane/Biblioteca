d39b14542b4c2d1c434c7121654f10e91ed6e7c8
Leilane <leilanericardo1@gmail.com>
2026-07-31 17:28:35 -0300

    Adição de busca por gênero

--- Biblioteca/src/main/java/com.leilane.Biblioteca/controller/BibliotecaController.java
+++ Biblioteca/src/main/java/com.leilane.Biblioteca/controller/BibliotecaController.java
@@ -1,11 +1,14 @@
 package com.leilane.Biblioteca.controller;
 
+import com.leilane.Biblioteca.model.Emprestimo;
 import com.leilane.Biblioteca.model.Livro;
+import com.leilane.Biblioteca.repository.EmprestimoRepository;
 import com.leilane.Biblioteca.repository.LivroRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -13,11 +16,12 @@ import java.util.List;
 public class BibliotecaController {
 
     private final LivroRepository livroRepository;
+    private final EmprestimoRepository emprestimoRepository;
 
 
-    public BibliotecaController(LivroRepository livroRepository) {
+    public BibliotecaController(LivroRepository livroRepository, EmprestimoRepository emprestimoRepository) {
         this.livroRepository = livroRepository;
-
+        this.emprestimoRepository = emprestimoRepository;
     }
 
     @GetMapping("/livros")
@@ -39,18 +43,20 @@ public class BibliotecaController {
 
     @DeleteMapping("/removeBook/{idToBeRemoved}")
     public void removeBook(@PathVariable int idToBeRemoved) {
-        if (livroRepository.existsById(idToBeRemoved)) {
-            livroRepository.deleteById(idToBeRemoved);
-        } else {
+        if (!livroRepository.existsById(idToBeRemoved)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Livro não existe");
         }
+        if(emprestimoRepository.existsByLivroId(idToBeRemoved)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O livro está emprestado e não pode ser removido.");
+        }
+        livroRepository.deleteById(idToBeRemoved);
         System.out.println("Livro " + idToBeRemoved + " Removido!");
 
     }
 
     @GetMapping("/livros/title/{title}")
     public List<Livro> findByTitle(@PathVariable String title) {
-        List<Livro> livros = livroRepository.findByTitleContainingIgnoreCase(title);
+        List<Livro> livros = livroRepository.findByTitleContaining(title);
         if(livros.isEmpty()){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum livro encontrado com esse título.");
         }
@@ -59,7 +65,7 @@ public class BibliotecaController {
 
     @GetMapping("/livros/author/{author}")
     public List<Livro> findByAuthor(@PathVariable String author){
-        List<Livro> livros = livroRepository.findByAuthorContainingIgnoreCase(author);
+        List<Livro> livros = livroRepository.findByAuthorContaining(author);
         if (livros.isEmpty()){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum autor encontrado com esse nome.");
         }
@@ -75,6 +81,12 @@ public class BibliotecaController {
         return livros;
     }
 
-
-
+    @GetMapping("/livros/gender/{genre}")
+    public List<Livro> findByGenre(@PathVariable String genre){
+        List<Livro> livros = livroRepository.findByGender(genre);
+        if(livros.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum livro do gênero foi encontrado.");
+        }
+        return livros;
+    }
 }
